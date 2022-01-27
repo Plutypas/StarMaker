92e3895fbde595b3cb1a940dac7245184c94a54a
Plutypas <93535957+Plutypas@users.noreply.github.com>
2022-01-27 22:29:45 +0000

    Fixed generateRelativePointMap() method in Star class

--- src/Star.java
+++ src/Star.java
@@ -15,11 +15,13 @@ public class Star {
 
     public ArrayList<int[]> generateRelativePointMap() {
         ArrayList<int[]> pointMap = new ArrayList<>();
+        int lastDegree=0;
         for (int i = 0; i < this.stopDistance; i++) {
-            int x1 = (int) (this.distance*Math.cos((this.degrees*i)%360));
-            int y1 = (int) (this.distance*Math.sin((this.degrees*i)%360));
-            int x2 = (int) (this.distance*Math.cos((this.degrees*(this.sepDegree+i))%360));
-            int y2 = (int) (this.distance*Math.sin((this.degrees*(this.sepDegree+i))%360));
+            int x1 = (int) (this.distance*Math.cos(Math.toRadians((lastDegree)%360)));
+            int y1 = (int) (this.distance*Math.sin(Math.toRadians((lastDegree)%360)));
+            int x2 = (int) (this.distance*Math.cos(Math.toRadians((lastDegree+(this.degrees*this.sepDegree))%360)));
+            int y2 = (int) (this.distance*Math.sin(Math.toRadians((lastDegree+(this.degrees*this.sepDegree))%360)));
+            lastDegree=((this.degrees*this.sepDegree)+lastDegree)%360;
             pointMap.add(new int[]{x1, y1, x2, y2});
         }
         return pointMap;
